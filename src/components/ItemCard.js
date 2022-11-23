a99c1d4b56c23b24d0e6647f750061a45c8e1864
Matias Garcia <garciamatias.159@gmail.com>
2022-11-23 07:53:24 -0300

    review2

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom"
 
 const ItemCard = ({escenario, cliente}) => {
     return (
-        <Link className="text-decoration-none col-6" to={`/video/${cliente}`}>
-        <div>
-            <div className="App-container-card d-flex justify-content-between">
-                <div style={{height:"200px"}} className="App-card">
-                    <div className="App-face App-face1 d-flex justify-content-center align-items-center">
-                        <div className="App-content">
-                            <h2 style={{marginBottom:'0'}} className="App-title p-3">{cliente}</h2>
+        <Link className="text-decoration-none col-12 col-md-5 mt-5" to={`/video/${cliente}`}>
+        <div style={{background:'#ff0057', height:'15rem', borderRadius:'1rem'}}>
+            <div className=" d-flex justify-content-between">
+                <div className="d-flex justify-content-center align-items-center flex-column">
+                    <div>
+                        <div>
+                            <h2 className="App-title fs-1 mt-5">{cliente}</h2>
                         </div>
                     </div>
-                    <div className="App-face App-face2">
-                        <div className="App-content d-felx justify-content-between flex-column align-items-center">
-                            <p className="text-black">Escenario: {escenario}</p>
+                    <div>
+                        <div>
+                            <p className="text-white text-center">Escenario: {escenario}</p>
                         </div>
                     </div>
                 </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -17,8 +17,8 @@ const Main = () => {
     }
 
     return (
-        <div className="m-5">
-            <div className="row container m-5">
+        <div style={{minHeight:'100vh'}}>
+            <div className="row container-fluid d-flex justify-content-between">
                 {data.map(el=> <ItemCard key={el.idVideo} {...el} />)}
             </div>
         </div>
